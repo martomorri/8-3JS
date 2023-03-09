19be1e4648f14a322618accbc895927acec72aa3
martomorri <106319796+martomorri@users.noreply.github.com>
2023-03-09 09:24:57 -0300

    9-3

--- scripts.js
+++ scripts.js
@@ -1,7 +1,29 @@
-var hincha = "boca";
-
-function esDeBoca(hincha) {
-    return hincha === "boca";
+function cambiarColor()
+{
+    document.getElementById("boton1").style.color = "blue";
 }
 
-console.log(esDeBoca(hincha));
+var inputUsuario = document.getElementById("usuario");
+inputUsuario.onkeyup = validarUsuario;
+
+function validarUsuario(event)
+{
+    console.log(event.target.value);
+
+    if (event.target.value.length < 8)
+    {
+        event.target.style.color = "red";
+    }
+    else
+    {
+        event.target.style.color = "green";
+    }
+}
+
+var boton = document.getElementById("boton");
+boton.onclick = enviado;
+
+function enviado(event)
+{
+    alert("Enviado");
+}
